62b1dd8c2893cfaade42982a5ce057e9e38c40b9
vijaynaidu013 <84774714+vijaynaidu013@users.noreply.github.com>
2021-10-29 15:24:14 +0530

    Update DemoController.java

--- src/main/java/com/example/demo/controller/DemoController.java
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -11,7 +11,7 @@ public class DemoController {
 @GetMapping(path="/hello")
 	public String hello()
 	{
-		return "Hello This is Maturity Visualizer Application";
+		return "Hello This is Maturity Visualizer Application1";
 	}
 	
 }
